1e7d73e3b2a3694e613d97507a682db508a456ac
gsvr <george@westerncapelabs.com>
2014-12-04 17:22:05 +0200

    basic flow without location functionality

--- go-app.js
+++ go-app.js
@@ -11,28 +11,124 @@ go.app = function() {
     var Choice = vumigo.states.Choice;
     var ChoiceState = vumigo.states.ChoiceState;
     var EndState = vumigo.states.EndState;
+    var FreeText = vumigo.states.FreeText;
+
 
     var GoApp = App.extend(function(self) {
-        App.call(self, 'states:start');
+        App.call(self, 'state_clinic_type');
+        var $ = self.$;
 
-        self.states.add('states:start', function(name) {
+        self.states.add('state_clinic_type', function(name) {
             return new ChoiceState(name, {
-                question: 'Hi there! What do you want to do?',
+                question:
+                    $("Welcome to Clinic Finder. What type of clinic " +
+                      "are you looking for?"),
 
                 choices: [
-                    new Choice('states:start', 'Show this menu again'),
-                    new Choice('states:end', 'Exit')],
+                    new Choice('nearest', $("Nearest Clinic")),
+                    new Choice('mmc', $("MMC Clinic")),
+                    new Choice('hct', $("HCT Clinic"))
+                ],
+
+                next: 'state_sim_type'
+            });
+        });
+
+        self.states.add('state_sim_type', function(name) {
+            return new ChoiceState(name, {
+                question:
+                    $("To find ur closest clinic we need to know " +
+                      "what SIM you have in ur phone:"),
+
+                choices: [
+                    new Choice('vodacom_mtn', $("Vodacom or MTN")),
+                    new Choice('other', $("Other")),
+                    new Choice('quit', $("Quit"))
+                ],
 
                 next: function(choice) {
-                    return choice.value;
+                    switch (choice.value) {
+                        case 'vodacom_mtn': return 'state_locate_permission';
+                        case 'other': return 'state_suburb';
+                        case 'quit': return 'state_quit';
+                    }
                 }
             });
         });
 
-        self.states.add('states:end', function(name) {
+        self.states.add('state_locate_permission', function(name) {
+            return new ChoiceState(name, {
+                question:
+                    $("Thanks! We will now locate your approximate " +
+                      "position and then send you an SMS with your " +
+                      "nearest clinic."),
+
+                choices: [
+                    new Choice('locate', $("Continue")),
+                    new Choice('no_locate', $("No don't locate me")),
+                ],
+
+                next: function(choice) {
+                    switch (choice.value) {
+                        case 'locate': return 'state_clinic_names';
+                        case 'no_locate': return 'state_reprompt_permission';
+                    }
+                }
+            });
+        });
+
+        self.states.add('state_reprompt_permission', function(name) {
+            return new ChoiceState(name, {
+                question:
+                    $("If you do not give consent we can't locate you " +
+                      "automatically. Alternatively, give us your " +
+                      "suburb:"),
+
+                choices: [
+                    new Choice('consent', $("Give consent")),
+                    new Choice('suburb', $("Give suburb")),
+                    new Choice('quit', $("Quit"))
+                ],
+
+                next: function(choice) {
+                    switch (choice.value) {
+                        case 'consent': return 'state_clinic_names';
+                        case 'suburb': return 'state_suburb';
+                        case 'quit': return 'state_quit';
+                    }
+                }
+            });
+        });
+
+        self.states.add('state_suburb', function(name) {
+            return new FreeText(name, {
+                question:
+                    $("To find your closest clinic we need to know " +
+                      "what suburb or area u are in. Please be " +
+                      "specific. e.g. Inanda Sandton"),
+
+                next: 'state_clinic_names'
+            });
+        });
+
+        self.states.add('state_clinic_names', function(name) {
             return new EndState(name, {
-                text: 'Thanks, cheers!',
-                next: 'states:start'
+                text:
+                    $("Clinic 1, Clinic 2"),
+
+                next: 'state_clinic_type'
+            });
+        });
+
+        self.states.add('state_quit', function(name) {
+            return new EndState(name, {
+                text:
+                    $("Thanks for using Clinic Finder. For info on " +
+                      "MMC visit brothersforlife.org. For info on " +
+                      "HCT visit zazi.org.za. Find a clinic on the " +
+                      "web visit healthsites.org.za"),
+
+                next: 'state_clinic_type'
             });
         });
     });
--- test/app.test.js
+++ test/app.test.js
@@ -23,50 +23,267 @@ describe("app", function() {
         });
 
         describe("when the user starts a session", function() {
-            it("should ask them what they want to do", function() {
+            it("should ask for type of clinic", function() {
                 return tester
-                    .start()
+                    .inputs(
+                        {session_event: "new"}
+                    )
                     .check.interaction({
-                        state: 'states:start',
+                        state: 'state_clinic_type',
                         reply: [
-                            'Hi there! What do you want to do?',
-                            '1. Show this menu again',
-                            '2. Exit'
+                            "Welcome to Clinic Finder. What type of clinic " +
+                            "are you looking for?",
+                            "1. Nearest Clinic",
+                            "2. MMC Clinic",
+                            "3. HCT Clinic"
                         ].join('\n')
                     })
                     .run();
             });
         });
 
-        describe("when the user asks to see the menu again", function() {
-            it("should show the menu again", function() {
+        describe("when the user selects a clinic type", function() {
+            it("should ask for type of sim card", function() {
                 return tester
-                    .setup.user.state('states:start')
-                    .input('1')
+                    .inputs(
+                        {session_event: "new"},
+                        '1'  // state_clinic_type
+                    )
                     .check.interaction({
-                        state: 'states:start',
+                        state: 'state_sim_type',
                         reply: [
-                            'Hi there! What do you want to do?',
-                            '1. Show this menu again',
-                            '2. Exit'
+                            "To find ur closest clinic we need to know " +
+                            "what SIM you have in ur phone:",
+                            "1. Vodacom or MTN",
+                            "2. Other",
+                            "3. Quit"
                         ].join('\n')
                     })
                     .run();
             });
         });
 
-        describe("when the user asks to exit", function() {
-            it("should say thank you and end the session", function() {
-                return tester
-                    .setup.user.state('states:start')
-                    .input('2')
-                    .check.interaction({
-                        state: 'states:end',
-                        reply: 'Thanks, cheers!'
-                    })
-                    .check.reply.ends_session()
-                    .run();
+        describe("when the user selects a sim type", function() {
+            describe("if the user selects 1. Vodacom or MTN", function() {
+                it("should confirm locating them", function() {
+                    return tester
+                        .inputs(
+                            {session_event: "new"},
+                            '1',  // state_clinic_type
+                            '1'  // state_sim_type
+                        )
+                        .check.interaction({
+                            state: 'state_locate_permission',
+                            reply: [
+                                "Thanks! We will now locate your approximate " +
+                                "position and then send you an SMS with your " +
+                                "nearest clinic.",
+                                "1. Continue",
+                                "2. No don't locate me"
+                            ].join('\n')
+                        })
+                        .run();
+                });
+
+                describe("if the user chooses 1. Continue", function() {
+                    it("should display clinic names", function() {
+                        return tester
+                            .inputs(
+                                {session_event: "new"},
+                                '1',  // state_clinic_type
+                                '1',  // state_sim_type
+                                '1'  // state_locate_permission
+                            )
+                            .check.interaction({
+                                state: 'state_clinic_names',
+                                reply:
+                                    "Clinic 1, Clinic 2"
+                            })
+                            .run();
+                    });
+                });
+
+                describe("if the user chooses 2. No don't locate", function() {
+                    it("should reprompt for location consent", function() {
+                        return tester
+                            .inputs(
+                                {session_event: "new"},
+                                '1',  // state_clinic_type
+                                '1',  // state_sim_type
+                                '2'  // state_locate_permission
+                            )
+                            .check.interaction({
+                                state: 'state_reprompt_permission',
+                                reply: [
+                                    "If you do not give consent we can't locate you " +
+                                    "automatically. Alternatively, give us your " +
+                                    "suburb:",
+                                    "1. Give consent",
+                                    "2. Give suburb",
+                                    "3. Quit"
+                                ].join('\n')
+                            })
+                            .run();
+                    });
+                });
+
+                describe("if the user replies after initially refusing consent", function() {
+
+                    describe("if they choose 1. Give consent", function() {
+                        it("should show 2 clinic names", function() {
+                            return tester
+                            .inputs(
+                                {session_event: "new"},
+                                '1',  // state_clinic_type
+                                '1',  // state_sim_type
+                                '2',  // state_locate_permission
+                                '1'  // state_reprompt_permission
+                            )
+                            .check.interaction({
+                                state: 'state_clinic_names',
+                                reply:
+                                    "Clinic 1, Clinic 2"
+                            })
+                            .run();
+                        });
+                    });
+
+                    describe("if they choose 2. Give suburb", function() {
+                        it("should prompt for their suburb", function() {
+                            return tester
+                            .inputs(
+                                {session_event: "new"},
+                                '1',  // state_clinic_type
+                                '1',  // state_sim_type
+                                '2',  // state_locate_permission
+                                '2'  // state_reprompt_permission
+                            )
+                            .check.interaction({
+                                state: 'state_suburb',
+                                reply:
+                                    "To find your closest clinic we need to know " +
+                                    "what suburb or area u are in. Please be " +
+                                    "specific. e.g. Inanda Sandton"
+                            })
+                            .run();
+                        });
+                    });
+
+                    describe("if they choose 3. Quit", function() {
+                        it("should show info and quit", function() {
+                            return tester
+                            .inputs(
+                                {session_event: "new"},
+                                '1',  // state_clinic_type
+                                '1',  // state_sim_type
+                                '2',  // state_locate_permission
+                                '3'  // state_reprompt_permission
+                            )
+                            .check.interaction({
+                                state: 'state_quit',
+                                reply:
+                                    "Thanks for using Clinic Finder. For info on " +
+                                    "MMC visit brothersforlife.org. For info on " +
+                                    "HCT visit zazi.org.za. Find a clinic on the " +
+                                    "web visit healthsites.org.za"
+                            })
+                            .check.reply.ends_session()
+                            .run();
+                        });
+                    });
+
+                });
+            });
+
+            describe("if the user selects 2. Other", function() {
+                it("should ask for their suburb", function() {
+                    return tester
+                        .inputs(
+                            {session_event: "new"},
+                            '1',  // state_clinic_type
+                            '2'  // state_sim_type
+                        )
+                        .check.interaction({
+                            state: 'state_suburb',
+                            reply:
+                                "To find your closest clinic we need to know " +
+                                "what suburb or area u are in. Please be " +
+                                "specific. e.g. Inanda Sandton"
+                        })
+                        .run();
+                });
+
+                describe("after entering their suburb", function() {
+                    it("should display two closest clinics", function() {
+                        return tester
+                            .inputs(
+                                {session_event: "new"},
+                                '1',  // state_clinic_type
+                                '2',  // state_sim_type
+                                'Observatory'  // state_suburb
+                            )
+                            .check.interaction({
+                                state: 'state_clinic_names',
+                                reply:
+                                    "Clinic 1, Clinic 2"
+                            })
+                            .check.reply.ends_session()
+                            .run();
+                    });
+                });
+            });
+
+            describe("if the user selects 3. Quit", function() {
+                it("should show info and end", function() {
+                    return tester
+                        .inputs(
+                            {session_event: "new"},
+                            '1',  // state_clinic_type
+                            '3'  // state_sim_type
+                        )
+                        .check.interaction({
+                            state: 'state_quit',
+                            reply:
+                                "Thanks for using Clinic Finder. For info on " +
+                                "MMC visit brothersforlife.org. For info on " +
+                                "HCT visit zazi.org.za. Find a clinic on the " +
+                                "web visit healthsites.org.za"
+                        })
+                        .check.reply.ends_session()
+                        .run();
+                });
             });
         });
+
+        // describe("when the user asks to see the menu again", function() {
+        //     it("should show the menu again", function() {
+        //         return tester
+        //             .setup.user.state('states:start')
+        //             .input('1')
+        //             .check.interaction({
+        //                 state: 'states:start',
+        //                 reply: [
+        //                     'Hi there! What do you want to do?',
+        //                     '1. Show this menu again',
+        //                     '2. Exit'
+        //                 ].join('\n')
+        //             })
+        //             .run();
+        //     });
+        // });
+
+        // describe("when the user asks to exit", function() {
+        //     it("should say thank you and end the session", function() {
+        //         return tester
+        //             .setup.user.state('states:start')
+        //             .input('2')
+        //             .check.interaction({
+        //                 state: 'states:end',
+        //                 reply: 'Thanks, cheers!'
+        //             })
+        //             .check.reply.ends_session()
+        //             .run();
+        //     });
+        // });
     });
 });
